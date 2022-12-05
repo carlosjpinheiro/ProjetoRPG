420682347d1e1c5bc99476b50513e64a72d11c17
Eduardo Floriani <eeduardofloriani@gmail.com>
2022-12-05 17:58:49 -0300

    player, menu and main

--- src/MainRPG.java
+++ src/MainRPG.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
 
 public class MainRPG {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Being heroi = new Character1();
+		Scanner scan = new Scanner(System.in);
+		Menu menu = new Menu();
+		Player player = new Player();
+		System.out.print("Nome do jogador: ");
+		String name = scan.next();
+		player.setName(name);
 		
-		Encounter.encounter(heroi);
-		
-		
-		
-		
+		try {
+			menu.history(player.getName());
+			menu.menu();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		
 		
 	}
-
 }
--- src/Utilities.java
+++ src/Utilities.java
@@ -1,11 +1,7 @@
-
 public class Utilities {
 
 	public static boolean chance(int prob) {
 		if((int)(Math.random()*100) <= prob) return true;
 		return false;
 	}
-	
-	
-	
 }
